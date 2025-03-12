5d86130b58cf806544b51acf2932a42765a4a50b
kgaur9586 <123kapilgaur9586@gmail.com>
2025-03-12 19:47:57 +0530

    web security config

--- examserver/src/main/java/com/exam/examserver/config/WebSecurityConfig.java
+++ examserver/src/main/java/com/exam/examserver/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig {
         http
         	.cors(cors -> cors.configurationSource(request -> {
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowedOrigins(List.of("http://localhost:4200"));
+            config.setAllowedOrigins(List.of("http://localhost:4200", "https://testsphere.onrender.com"));
             config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             config.setAllowedHeaders(List.of("*"));
             config.setAllowCredentials(true);
