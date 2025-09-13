4b6c80c6afaba72df94e0312fbab7f359834a65d
kapil <kapil.4waytechnologies@gmail.com>
2025-09-13 23:38:09 +0530

    Update quiz navigation links to use 'id' instead of 'qid' for consistency; refactor user profile display to show roles correctly

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { Component, ElementRef,HostListener, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,64 +15,83 @@ import { QuizService } from '../../services/quiz.service';
 import { CategoryService } from '../../services/category.service';
 import { error } from 'console';
 
-
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule,MatListModule,MatCardModule,MatButtonModule,MatIconModule,RouterLink],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterLink,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent implements OnInit{
-  
+export class HomeComponent implements OnInit {
   quizzes: any;
-  category:any;
+  category: any;
   categoryQuizzes: { [key: string]: any[] } = {};
 
-    constructor(private route:ActivatedRoute,private quizService:QuizService,private catService:CategoryService){}
-    ngOnInit(): void {
-      this.quizService.getActiveQuizzes().subscribe((data:any) => {
-      this.quizzes = data;
-    },(error:any)=> {
-            console.log(error);
-        });
-        this.catService.categories().subscribe((data:any)=>{
-          this.category = data;
-          this.loadQuizzesForCategories();
-        },(error:any)=>{
-          console.log(error);
-        });
-    }
-    loadQuizzesForCategories() {
-      if (!this.category) return;
-      this.category.forEach((c: any) => {
-        if (c.cid) {
-          this.quizService.getActiveQuizzesOfCategory(c.cid).subscribe(
-            (data: any) => {
-              if(data != null && data.length > 0){
-
-                this.categoryQuizzes[c.cid] = this.categoryQuizzes[c.cid] || [];
-                this.categoryQuizzes[c.cid] = data;
-              }
-              
-            },
-            (error: any) => {
-              console.log(error);
+  constructor(
+    private route: ActivatedRoute,
+    private quizService: QuizService,
+    private catService: CategoryService
+  ) {}
+  ngOnInit(): void {
+    console.log('home component loaded');
+    this.quizService.getActiveQuizzes().subscribe(
+      (data: any) => {
+        this.quizzes = data;
+        console.log('quizzes - ', this.quizzes);
+      },
+      (error: any) => {
+        console.log('error loading homecomponent', error);
+      }
+    );
+    this.catService.categories().subscribe(
+      (data: any) => {
+        this.category = data;
+        this.loadQuizzesForCategories();
+      },
+      (error: any) => {
+        console.log(error);
+      }
+    );
+  }
+  loadQuizzesForCategories() {
+    if (!this.category) return;
+    this.category.forEach((c: any) => {
+      if (c.cid) {
+        this.quizService.getActiveQuizzesOfCategory(c.cid).subscribe(
+          (data: any) => {
+            if (data != null && data.length > 0) {
+              this.categoryQuizzes[c.id] = this.categoryQuizzes[c.id] || [];
+              this.categoryQuizzes[c.id] = data;
             }
-          );
-        }
-      });
-    }
-  
+          },
+          (error: any) => {
+            console.log(error);
+          }
+        );
+      }
+    });
+  }
 
   @ViewChild('scrollContainer', { static: false }) scrollContainer!: ElementRef;
   scrollLeft() {
-    this.scrollContainer.nativeElement.scrollBy({ left: -280, behavior: 'smooth' });
+    this.scrollContainer.nativeElement.scrollBy({
+      left: -280,
+      behavior: 'smooth',
+    });
   }
 
   // Scroll right
   scrollRight() {
-    this.scrollContainer.nativeElement.scrollBy({ left: 280, behavior: 'smooth' });
+    this.scrollContainer.nativeElement.scrollBy({
+      left: 280,
+      behavior: 'smooth',
+    });
   }
-  
 }
